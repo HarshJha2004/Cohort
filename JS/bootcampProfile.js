448b68bfd0892891b1fbf14a878c153af43330f0
Harsh Jha <Harshwork5546@gmail.com>
2025-07-23 09:04:57 +0530

    Datatype Completed

---
+++ JS/bootcampProfile.js
@@ -0,0 +1,37 @@
+// JS Lecture - 2.
+// Datatypes in JS.
+
+ // Primitive datatype.
+    //String - "", '', ``.
+    let name = "Harsh Jha"
+
+    //Number - 25 (int), 25.9 (float)
+    let age = 20
+
+    //Boolean
+    let isPaid = false  
+
+    // Null(khali) - apne aap me ek value hai, for more better understanding the weather example.
+    let favouriteClass = null;
+
+    // undefined (Baadme me dekhenge) - Na koi number dala, na kuch dusri value.
+    let hometown;
+     
+    // BigInt
+    // symbol(will learn in React)
+
+
+  // Non-primitive datatype / Object.
+    // array
+    let skills = ["HTML", "CSS", "JS"]
+
+    // Object
+    let studentProfile = {
+        name: "Harsh Jha",
+        age: 20,
+        isPaid: false,
+        skills: ["HTML", "CSS", "JS"],
+        favouriteClass: null,
+    };
+
+    // JS ek OOP language hai isme sab Object ki tarah store hote hai.
