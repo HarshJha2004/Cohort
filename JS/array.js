7bfccb22bed51ad6f3e415f715718e24fe076eed
Harsh Jha <Harshwork5546@gmail.com>
2025-07-23 16:09:51 +0530

    array completed

---
+++ JS/array.js
@@ -0,0 +1,14 @@
+//   create array
+  let fruits = ["Apple", "Mango", "Banana"];
+  let intFruits = new Array("kiwi", "avacado", "dragonfruit");  
+
+  console.log(fruits);
+  console.log(fruits[0]);
+
+  fruits[0] = "blueberry";
+  console.log(fruits);
+  
+  console.log(fruits.length);
+  
+  
+  
