1b865ee0697a09b155495397d6f27d2f0851ac14
Harsh Jha <Harshwork5546@gmail.com>
2025-07-30 16:51:46 +0530

    Objects Lessons

--- JS/Ds-3.js
+++ JS/Ds-3.js
@@ -8,11 +8,13 @@
         Hobbies : ["GYM", "Coding", "Reading"],
         isMarried : false,
         hasGF : true,
+
         //and we can also create the function.
         getFullName : function(){
             return 'Harsh Jha'
         }, 
 
+        //nested Object(object ke andar object)
         address : {
             State:"GJ",
             City:"Surat",
@@ -20,6 +22,35 @@
         }
     }
 
-    // In object when we want to access any value then the syntax is (person.keyName).
-    console.log(person.Hobbies.indexOf("Reading"));
+        // Change the key values.
+            person.name = 'Monkey Bajrangi Vlogger'
+
+    // In object when we want to access any value then the syntax is (arrayName.keyName).
+    // console.log(person.Hobbies.indexOf("Reading"));
+    // console.log(person.name.);
+    
+
+
+    let p1 = {
+        fname : "JS",
+        lname : "Scipt"
+    }
+
+   
+    // console.log(p2);
+    // console.log(p1);
+
+    // To copy a actual Object, not his memory address.
+    // let p2 = {
+    //     ...p1 //spread opertor which is copy all the actual values of P1. not a memory address.but the problem is that of spread operator is vo inner (nested) Objects pe kaam nhi karta.
+    // }
+
+    // Objects ka Deep Copy.
+        // Object se string
+        const p1kastring = JSON.stringify(p1)
+
+        // String se phir object.
+        let p2 = JSON.parse(p1kastring)
+
+        
     
