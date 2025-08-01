a087ede7d5fea38f50bf7affc44ba9ad3b45dd0b
Harsh Jha <Harshwork5546@gmail.com>
2025-08-01 22:21:07 +0530

    function-lesson part-1 completed

---
+++ JS-Revision/Lecture-1.js
@@ -0,0 +1,18 @@
+function printChai(){
+    console.log("Hello chai");
+}
+
+
+function bringBrush(kitne){
+    console.log("Haanji, le ayye brush");
+}
+
+// Yaha hamne factory ko data diya but factory ko bhi to data lena padega.
+bringBrush(4);
+
+
+function addTwo(ekNum, doNum){
+    return ekNum + doNum;
+}
+
+addTwo(2, 2)
