0fc43cb83a6841f0212267b002a2a176bf9f6a15
Harsh Jha <Harshwork5546@gmail.com>
2025-08-02 11:54:06 +0530

    Basic datatypes complete

---
+++ JS-Revision/Lec-2.js
@@ -0,0 +1,34 @@
+//primitive datatypes.
+    // string => "", '', ``
+        let name = "Bajrangi"
+
+    // Number -> int, float
+        let age = 12;
+
+    // Boolean -> true/false
+        let isPaid = true;
+
+    // Null
+        let favouriteClass = null;
+
+    // undefined
+        let wifeName;
+
+    // symbol
+    // Bigint
+
+
+
+//non-Primitive / Objects.
+    // array
+        let skills = ["HTML", "CSS", "JS"]
+
+    //Object
+        let studentProfile = {
+            name : "Harsh",
+            age : 21,
+            isPaid : true,
+            skills : ["MERN"],
+            favouriteClass : null
+        }
+
